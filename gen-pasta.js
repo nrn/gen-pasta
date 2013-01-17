0413e7127c8b77160752b4b81a14205d8615aa10
Nick Niemeir <nick.niemeir@gmail.com>
2013-01-16 23:15:16 -0700

    Add last(any)

--- gen-pasta.js
+++ gen-pasta.js
@@ -11,8 +11,19 @@ function genPasta (opts) {
     if (console && console.log) console.log(arrify(arguments))
   }
 
+  function last(item) {
+    var num = false
+    if (typeof item === 'number') return +item.toString().split('').pop()
+    if (typeof item === 'string') return item.split('').pop()
+    if (Array.isArray(item)) return item.pop()
+    if (typeof item === 'object') return Object.keys(item).pop()
+    return item
+
+  }
+
   return { log: log
     , l: log
+    , last:last
     , arrify: arrify
     }
 
--- t/gen-pasta.js
+++ t/gen-pasta.js
@@ -2,7 +2,7 @@ var p = require('../gen-pasta')()
   , test = require('tape')
 
 test('General', function (t) {
-  t.plan(2)
+  t.plan(6)
 
   t.doesNotThrow(p.log, 'Log does not throw')
 
@@ -12,5 +12,14 @@ test('General', function (t) {
     , [1, 2, 3].join()
     , 'Turn array like objects into arrays'
   )
+
+  t.equal(p.last([1, 2, 3]), 3, 'Last element')
+
+  t.equal(p.last({ c: 3, a: 1, b: 2}), 'b', 'Last key')
+
+  t.equal(p.last('asdf'), 'f', 'Last character')
+
+  t.equal(p.last(1234), 4, 'Last diget')
+
 })
 
